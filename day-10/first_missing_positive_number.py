39beba05957560398d7570b8e9aa5d61439f01f5
anmoldua33 <93073455+anmoldua33@users.noreply.github.com>
2022-04-05 00:43:50 +0530

    Add files via upload

---
+++ day-10/first_missing_positive_number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums=set(nums)
+        ans=1
+        while ans in nums:
+            ans+=1
+        return ans
